ba216271bd2bccaccc5756cc5151dbdc4f0db06b
rhinocrose938 <57975516+rhinocrose938@users.noreply.github.com>
2020-09-28 22:46:25 +0530

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,13 +5,13 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 class Table extends React.Component {
-    
+
     render()  {
 
-    const  requiredData = this.props.data1; // Array of four objects
-    
+    const  requiredData = this.props.tableData; // Array of four objects
+
     /*passing the data recieved from Timeline component which is array of four objects to AGridReact component*/
-       
+
      const columns = Object.keys(requiredData[0]).map( e => ({headerName: e.toUpperCase(), field: e}));
 
      const defaultColDef = { sortable: true, editable: true,  filter: true, resizable: true, width: 120}
@@ -21,8 +21,8 @@ class Table extends React.Component {
     return (
         <div className="ag-theme-alpine container m-3 p-3" style={{height: 300, width: 760}}>
             <h4 style={{color: 'white'}}>Risk Category Summary</h4>
-               <AgGridReact  onGridReady={onGridReady}   
-                             rowData={requiredData} columnDefs={columns} 
+               <AgGridReact  onGridReady={onGridReady}
+                             rowData={requiredData} columnDefs={columns}
                              defaultColDef={defaultColDef}/>
           </div>
       );
