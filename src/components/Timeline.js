8c6acfa2da5f0794a5d5d81db795210140f77ae9
rhinocrose938 <57975516+rhinocrose938@users.noreply.github.com>
2020-09-28 22:46:44 +0530

    Update Timeline.js

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,17 +6,17 @@ import NewChart from './NewChart';
 
 class Timeline extends Component {
 
-    
+
 
     render() {
 
-     const categoryData = data.defaultReport.aggregated.items; 
+     const categoryData = data.defaultReport.aggregated.items;
      // array of 4 objects passed as props to Table component
-     
-     const timebasedData = data.defaultReport.items; 
+
+     const timebasedData = data.defaultReport.items;
      // array of 30 objects
 
-     
+
         return (
             <div className="container m-4 ">
                <div className="row m-5">
@@ -24,22 +24,22 @@ class Timeline extends Component {
                   <div className="col-sm-7">
                     <div className="card">
                        <div className="card-body">
-                       <NewChart data3 ={timebasedData}/>
+                       <NewChart chartData ={timebasedData} categoryData={categoryData}/>
                       </div>
                      </div>
                   </div>
                   <div className="col-sm-2">
                   </div>
               </div>
-              
+
                 <div className="row m-5">
                   <div className="col-sm-1"></div>
-                    <div className="col-sm-11"> 
-                    <Table data1 = {categoryData}/>
+                    <div className="col-sm-11">
+                    <Table tableData = {categoryData}/>
                   </div>
                       <div className="col-sm-0"></div>
                   </div>
-                  
+
             </div>
         )
     }
